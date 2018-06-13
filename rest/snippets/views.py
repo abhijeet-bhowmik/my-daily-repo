c8231d3d86559cf415dfa207fbf7486b04d28c57
abhijeet-newput <=>
2018-06-13 16:39:40 +0530

    removed api views function

--- rest/snippets/views.py
+++ rest/snippets/views.py
@@ -11,46 +11,46 @@ from rest_framework import status
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
-def snippet_list(request):
-	if request.method == 'GET':
-		snippets = Snippet.objects.all()
-		serializer = SnippetSerializer(snippets, many=True)
-		return Response(serializer.data)
+# @api_view(['GET', 'POST'])
+# def snippet_list(request):
+# 	if request.method == 'GET':
+# 		snippets = Snippet.objects.all()
+# 		serializer = SnippetSerializer(snippets, many=True)
+# 		return Response(serializer.data)
 
-	elif request.method == 'POST':
-		data = JSONParser().parse(request)
-		serializer = SnippetSerializer(data=data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		print("error # 1")
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# 	elif request.method == 'POST':
+# 		data = JSONParser().parse(request)
+# 		serializer = SnippetSerializer(data=data)
+# 		if serializer.is_valid():
+# 			serializer.save()
+# 			return Response(serializer.data, status=status.HTTP_201_CREATED)
+# 		print("error # 1")
+# 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_details(request, pk):
-	#retrieve the snippet using primary key
-	try:
-		snippet = Snippet.objects.get(pk=pk)
-	except:
-		print("error # 2")
-		return Response(status=status.HTTP_404_NOT_FOUND)
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def snippet_details(request, pk):
+# 	#retrieve the snippet using primary key
+# 	try:
+# 		snippet = Snippet.objects.get(pk=pk)
+# 	except:
+# 		print("error # 2")
+# 		return Response(status=status.HTTP_404_NOT_FOUND)
 
-	if request.method == 'GET':
-		serializer = SnippetSerializer(snippet)
-		return Response(serializer.data)
+# 	if request.method == 'GET':
+# 		serializer = SnippetSerializer(snippet)
+# 		return Response(serializer.data)
 
-	elif request.method == 'PUT':
-		print (request.body);
-		stream = BytesIO(request.body)
-		data = JSONParser().parse(stream)
-		serializer = SnippetSerializer(snippet, data=data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
-		print("Error # 3")
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# 	elif request.method == 'PUT':
+# 		print (request.body);
+# 		stream = BytesIO(request.body)
+# 		data = JSONParser().parse(stream)
+# 		serializer = SnippetSerializer(snippet, data=data)
+# 		if serializer.is_valid():
+# 			serializer.save()
+# 			return Response(serializer.data)
+# 		print("Error # 3")
+# 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-	elif request.method == 'DELETE':
-		snippet.delete()
-		return Response(status=HTTP_204_NO_CONTENT)
+# 	elif request.method == 'DELETE':
+# 		snippet.delete()
+# 		return Response(status=HTTP_204_NO_CONTENT)
