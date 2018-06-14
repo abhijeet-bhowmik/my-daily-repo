c4c826ba1f92cea321660a8480a975f8fc83180b
abhijeet-newput <=>
2018-06-14 07:16:52 +0530

    a few tweaks

--- rest/snippets/views.py
+++ rest/snippets/views.py
@@ -66,12 +66,48 @@ from rest_framework import APIView
 
 class SnippetList(APIView):
 
-	def get(request):
+	def get(self,request, format=None):
 		snippets = Snippet.objects.all()
 		serializer = SnippetSerializer(snippets, many=True)
 		return Response(serializer.data)
 
-	def post(request):
+	def post(self,request, format=None):
+		data = JSONParser().parse(request.body)
+		serializer = SnippetSerializer(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(status=HTTP_201_CREATED)
+		return Response(status=HTTP_400_BAD_REQUEST)
+
+
+class SnippetDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 
 
